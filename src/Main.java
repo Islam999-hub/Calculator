96a76be39681dcad1048b7ae26343b3e640da053
Islam Makhauri <islam999.osago@gmail.com>
2024-08-14 21:24:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+    public static void main(String [] args){
+
+       System.out.println("HELLO");
+
+
+
+
+    }
+
+    public final class Solution {
+
+        public String toLowerCase(String input) {
+            String str = input;
+            String str2 = str.toLowerCase();
+            return str2;
+        }
+    }
+
+
+
+
+}
